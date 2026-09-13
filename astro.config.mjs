eeee2d3e1d02cd7618f50be26c7e8f7e5a2fb186
Andre Wahyu Hermawan <wahyuandre007@gmail.com>
2026-09-14 06:40:04 +0700

    chore: remove public/studio and update site url in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
- site: 'https://andrean-lp.github.io',
+  site: 'https://andrean-lp.github.io',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
   markdown: {
     extendDefaultPlugins: true,
