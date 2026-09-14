b4a4fbb04345c43c5a575b9cdbabe46e9fe3f625
Andre Wahyu Hermawan <wahyuandre007@gmail.com>
2026-09-14 08:03:38 +0700

    fix: remove Partytown deprecated APIs, add profile CMS database and high-res proportional avatar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://andrean-lp.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), partytown()],
+  integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [[autoNewTabExternalLinks, {
